90f6e2cbf0dba3fe7cb17d2560c6798571af34ed
Andrei Zaharevich <zaharevich.andrzej@gmail.com>
2025-04-05 10:50:59 +0300

    [#2] Got rid of all unnecessary files and Arkanoid code, installed Waiting screen

--- Rogalique/LevelState.h
+++ Rogalique/LevelState.h
@@ -1,16 +1,11 @@
 #pragma once
 #include "SFML/Graphics.hpp"
 #include <vector>
-#include "Block.h"
-#include "LevelLoader.h"
+
 
 namespace Rogalique
 {
     struct LevelState {
-        sf::Vector2f platePosition;
-        sf::Vector2f ballPosition;
-        std::vector<std::shared_ptr<Block>> blocks;
-        int score;
-        std::vector<std::pair<BonusType, sf::Vector2f>> bonuses;
+
     };
 }
